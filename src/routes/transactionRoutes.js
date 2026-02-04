0c7c213866f7248a54d208daea3bae9d05712153
Dharshana-11 <dharshana1103@gmail.com>
2026-02-04 20:02:04 +0530

    feat: added get all transactions routes

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createTransactionController,
   updateTransactionController,
+  getAllTransactionsController,
 } from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", createTransactionController);
 router.put("/:id", updateTransactionController);
+router.get("/", getAllTransactionsController);
 
 export default router;
