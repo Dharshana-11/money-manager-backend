b410e3bbaf8be3ce94e9f6bf841b421aa8d6f421
Dharshana-11 <dharshana1103@gmail.com>
2026-02-04 19:41:18 +0530

    feat: added routes for update transaction

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createTransactionController } from "../controllers/transactionController.js";
+import {
+  createTransactionController,
+  updateTransactionController,
+} from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", createTransactionController);
+router.put("/:id", updateTransactionController);
 
 export default router;
