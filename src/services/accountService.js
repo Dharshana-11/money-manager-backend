d58f49c801514e732759111ba6e3db452c1e2477
Dharshana-11 <dharshana1103@gmail.com>
2026-02-04 11:28:38 +0530

    refactor: generalized names in service

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -3,32 +3,27 @@ import { ACCOUNT_TYPES } from "../utils/constants.js";
 
 const createAccount = async (accountData) => {
   // 1. destructure values
-  const {
-    accountName,
-    accountType,
-    accountDescription,
-    accountBalance = 0,
-  } = accountData;
+  const { name, type, description, balance = 0 } = accountData;
 
   // 2. validate name
-  if (!accountName) {
+  if (!name) {
     throw new Error("Account name is required!");
   }
   // 3. validate type using ACCOUNT_TYPES
-  if (!ACCOUNT_TYPES.includes(accountType)) {
+  if (!ACCOUNT_TYPES.includes(type)) {
     throw new Error("Account type invalid!");
   }
 
-  if (accountBalance < 0) {
+  if (balance < 0) {
     throw new Error("Account balance cannot be negative");
   }
 
   // 4. create account using Account model
   const account = await Account.create({
-    name: accountName,
-    type: accountType,
-    description: accountDescription,
-    balance: accountBalance,
+    name,
+    type,
+    description,
+    balance,
   });
 
   // 5. return created account
